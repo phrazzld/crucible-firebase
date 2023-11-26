33c82f5c43d9fb18387123daaf4cf603b3461bd8
phaedrus <phaedrus.raznikov@pm.me>
2023-11-26 15:24:37 -0600

    chore(oncall): stub

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,27 @@
-import { onRequest } from "firebase-functions/v2/https";
+import * as admin from 'firebase-admin';
 import * as logger from "firebase-functions/logger";
-/* import { onCall } from "firebase-functions/v2/https"; */
+import { onCall, onRequest } from 'firebase-functions/v2/https';
+import serviceAccount from '../service-account-key.json';
 
-export const helloWorld = onRequest((request, response) => {
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+})
+
+const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", { structuredData: true });
   logger.info("request:", request)
 
   response.send("Hello from Firebase!");
 });
+
+const callMeMaybe = onCall(async (request) => {
+  logger.info("callMeMaybe:", request)
+
+  return {
+    data: "Hello from Firebase!",
+    ps: "I just met you",
+  }
+})
+
+exports.callMeMaybe = callMeMaybe;
+exports.helloWorld = helloWorld;
